b03a19e450f6f20b694dc9411bb1271b74aaf4e5
Vandit Dubey <77433090+Shrillx@users.noreply.github.com>
2022-06-03 15:49:47 +0530

    Create lastIndex.java

---
+++ Day - 03/lastIndex.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+       Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i]= scan.nextInt();
+        }
+        int x = scan.nextInt();
+
+        System.out.println(lastIndex(arr,arr.length-1,x));
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x){
+        if(idx == -1)
+        {
+            return -1;
+        }
+
+        if(arr[idx] == x)
+        {
+            return idx;
+        }
+
+        return lastIndex(arr, idx-1,x);
+    }
+
+}
