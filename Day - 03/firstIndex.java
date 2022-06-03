def709dd8ee9af00e0f1180c7d4c24edeec82e5a
Vandit Dubey <77433090+Shrillx@users.noreply.github.com>
2022-06-03 15:47:04 +0530

    Create firstIndex.java

---
+++ Day - 03/firstIndex.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+
+    public static void main(String[] args) throws Exception 
+    {      
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i]= scan.nextInt();
+        }
+        int x = scan.nextInt();
+
+        System.out.println(firstIndex(arr,0,x));
+    }
+    
+
+    public static int firstIndex(int[] arr, int idx, int x){
+        if(idx == arr.length)
+        {
+            return -1;
+        }
+
+        if(arr[idx] == x)
+        {
+            return idx;
+        }
+        return firstIndex(arr,idx+1,x);
+       
+    }
+
+}
